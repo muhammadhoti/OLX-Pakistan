55bf579090752fc045a23a532a54db8e5a0ca33e
Muhammad Hoti <muhammad.hoti@gmail.com>
2018-08-02 01:41:03 +0500

    Fetching For Favourites Done By Child Added Method

--- favourite.js
+++ favourite.js
@@ -19,6 +19,9 @@ function signOut(){
         document.getElementById("userProfile").innerHTML=firebase.auth().currentUser.displayName;
         document.getElementById(`suh`).innerHTML = `Ads Favourite By ${firebase.auth().currentUser.displayName}`
         document.getElementById("userProfile").style.display="block";
+        //Calling Function Of Fetching 
+        fetchFavourites();
+        
       } else {
         // No user is signed in.
       } 
@@ -33,6 +36,51 @@ function signOut(){
       });
     }
     
+    //fetching favourites 
+    var database = firebase.database();
+    function fetchFavourites(){
+    
+    var favouritesRef = database.ref('favourites/' + firebase.auth().currentUser.uid);
+    favouritesRef.on('child_added', function (data) {
+      // console.log(data.val());
+      adCard(data.val(), data.key);
+      document.getElementById("row").innerHTML += adCard(data.val(), data.key);
+      // table.innerHTML += row;
+      // addCommentElement(postElement, data.key, data.val().text, data.val().author);
+    });
+  }
+
+  //generateAdCard
+  function adCard(data, key){
+    return`
+     <div class="cardstyling col-lg-4 col-sm-6 portfolio-item">
+       <div class="card h-100">
+         <small>${data.displayName}</small>
+         <img class="validate card-img-top" src=${data.url} />
+         <div class="card-body">
+           <h3 class="card-title">${data.title}</h3>
+           <h4 class="category">${data.category}</h4>
+           <p class="validate card-text">${data.description}</p>
+           <h5>${data.price}</h5>
+           <button type="button" class="btn btn-danger" onclick="deleteFavourite('${key}',this)">Delete Favourite</button>
+         </div>
+       </div>
+     </div>
+    `
+     }
+
+  //Delete Favourite 
+
+  function deleteFavourite(key, button) {
+    
+    // console.log(button.parentElement)
+    document.getElementById('row').removeChild(button.parentElement.parentElement.parentElement);
+    // var remove = button.parentElement.parentElement;
+    // document.getElementById(`row`).removeChild(remove);
+    var favouritesRef = database.ref('favourites/' + firebase.auth().currentUser.uid + `/` + key).set({});
+    
+  }  
+
     //search function
     function searchFunction() {
       var search = document.getElementById('search');
@@ -51,30 +99,32 @@ function signOut(){
       }
     }
   
-  //HomePageCategoySelection
+
   
-//   function selectCategory() {
-//     // var categoryOnHomepage = document.getElementById(`homePageCategorySelection`);
-//     // categoryOnHomepage.options[categoryOnHomepage.selectedIndex].value;
-//     var selectCategory = document.getElementById(`homePageCategorySelection`);
-//     selectCategory.options[selectCategory.selectedIndex].value;
-//     var categoryDivs =document.getElementsByClassName(`category`);
+  //CategoySelection
   
-//     for(i=0 ;i<categoryDivs.length ;i++){
+  function selectCategory() {
+    // var categoryOnHomepage = document.getElementById(`homePageCategorySelection`);
+    // categoryOnHomepage.options[categoryOnHomepage.selectedIndex].value;
+    var selectCategory = document.getElementById(`homePageCategorySelection`);
+    selectCategory.options[selectCategory.selectedIndex].value;
+    var categoryDivs =document.getElementsByClassName(`category`);
+  
+    for(i=0 ;i<categoryDivs.length ;i++){
   
       
-//       // console.log(categoryDivs[i].innerHTML);
-//       if(selectCategory.options[selectCategory.selectedIndex].value === `All Categories`){
-//         categoryDivs[i].parentElement.parentElement.parentElement.style.display="";
-//       }
-//       else if(selectCategory.options[selectCategory.selectedIndex].value === `${categoryDivs[i].innerHTML}`){
-//         // console.log(categoryDivs[i].innerHTML)
-//         categoryDivs[i].parentElement.parentElement.parentElement.style.display="";
-//       }
-//       else{
-//         categoryDivs[i].parentElement.parentElement.parentElement.style.display="none";
-//       }
-//     }
-//   }
+      // console.log(categoryDivs[i].innerHTML);
+      if(selectCategory.options[selectCategory.selectedIndex].value === `All Categories`){
+        categoryDivs[i].parentElement.parentElement.parentElement.style.display="";
+      }
+      else if(selectCategory.options[selectCategory.selectedIndex].value === `${categoryDivs[i].innerHTML}`){
+        // console.log(categoryDivs[i].innerHTML)
+        categoryDivs[i].parentElement.parentElement.parentElement.style.display="";
+      }
+      else{
+        categoryDivs[i].parentElement.parentElement.parentElement.style.display="none";
+      }
+    }
+  }
     
     
